57198c3b55c1dff6b7293a5b7ca6a6411439cc93
A-Lari <99265125+A-Lari@users.noreply.github.com>
2022-07-08 18:29:34 +0200

    Full project v1

---
+++ components/Add.jsx
@@ -0,0 +1,143 @@
+import { useState } from "react";
+import styles from "../styles/Add.module.css";
+import axios from "axios";
+import { useRouter } from "next/router";
+
+const Add = ({ setClose }) => {
+  const [file, setFile] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [desc, setDesc] = useState(null);
+  const [prices, setPrices] = useState([]);
+  const [extraOptions, setExtraOptions] = useState([]);
+  const [extra, setExtra] = useState(null);
+
+  const changePrice = (e, index) => {
+    const currentPrices = prices;
+    currentPrices[index] = e.target.value;
+    setPrices(currentPrices);
+  };
+
+  const handleExtraInput = (e) => {
+    setExtra({ ...extra, [e.target.name]: e.target.value });
+  };
+
+  const handleExtra = (e) => {
+    setExtraOptions((prev) => [...prev, extra]);
+  };
+
+  const handleCreate = async () => {
+    const data = new FormData();
+    data.append("file", file);
+    data.append("upload_preset", "uploads");
+    try {
+      const uploadRes = await axios.post(
+        "https://api.cloudinary.com/v1_1/dcxuolsow/image/upload",
+        data
+      );
+
+      console.log(uploadRes.data);
+
+      const { url } = uploadRes.data;
+      const newProduct = {
+        title,
+        desc,
+        prices,
+        extraOptions,
+        img: url,
+      };
+
+      await axios.post("http://localhost:3000/api/products", newProduct);
+      setClose(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <span onClick={() => setClose(true)} className={styles.close}>
+          X
+        </span>
+        <h1>Ajouter une nouvelle pizza</h1>
+        <div className={styles.item}>
+          <label className={styles.label}>Choisir une photo</label>
+          <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Titre</label>
+          <input
+            className={styles.input}
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Description</label>
+          <textarea
+            rows={4}
+            type="text"
+            onChange={(e) => setDesc(e.target.value)}
+          />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Prix</label>
+          <div className={styles.priceContainer}>
+            <input
+              className={`${styles.input} ${styles.inputSm}`}
+              type="number"
+              placeholder="Classico"
+              onChange={(e) => changePrice(e, 0)}
+            />
+            <input
+              className={`${styles.input} ${styles.inputSm}`}
+              type="number"
+              placeholder="Generoso"
+              onChange={(e) => changePrice(e, 1)}
+            />
+            <input
+              className={`${styles.input} ${styles.inputSm}`}
+              type="number"
+              placeholder="Gigantessa"
+              onChange={(e) => changePrice(e, 2)}
+            />
+          </div>
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Extra</label>
+          <div className={styles.extra}>
+            <input
+              className={`${styles.input} ${styles.inputSm}`}
+              type="text"
+              placeholder="Item"
+              name="text"
+              onChange={handleExtraInput}
+            />
+            <input
+              className={`${styles.input} ${styles.inputSm}`}
+              type="number"
+              placeholder="Price"
+              name="price"
+              onChange={handleExtraInput}
+            />
+            <button className={styles.extraButton} onClick={handleExtra}>
+              Ajouter
+            </button>
+          </div>
+          <div className={styles.extraItems}>
+            {extraOptions.map((option) => (
+              <span key={option.text} className={styles.extraItem}>
+                {option.text}
+              </span>
+            ))}
+          </div>
+        </div>
+        <button className={styles.addButton} onClick={handleCreate}>
+          Créer
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Add;
